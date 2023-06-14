ffd5f70a70fbe606f8b1a3a8b2cf0ddd8ed92dd6
Erick Medeiros <erick.medeiros.104@ufrn.edu.br>
2023-06-13 21:48:42 -0300

    build: :tada: initial landing page commit

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Message: React.FC = () => {
+  return (
+    <section>
+      <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="bg-neutral-500 px-8 py-24 md:p-12 lg:px-16 lg:py-24">
+            <div className="mx-auto max-w-xl">
+              <h2 className="text-2xl italic text-white md:text-3xl">
+                “Os amigos são uma das nossas maiores riquezas, devem ser
+                cultivados, com muito cuidado e ternura.”
+              </h2>
+
+              <p className="hidden font-bold text-white/90 sm:mt-4 sm:block text-right">
+                Carlo Acutis
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
+            <img
+              alt="Student"
+              src="https://i.pinimg.com/736x/ce/88/c3/ce88c3eb2f03b319d4b90e19999043d6.jpg"
+              className="h-40 w-full object-cover sm:h-56 md:h-full"
+            />
+            <img
+              alt="Student"
+              src="https://segueme.org/wp-content/uploads/2019/02/logo.png"
+              className="h-40 w-full object-scale-down sm:h-56 md:h-full bg-primary"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Message;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FFB819",
+          secondary: "#8A4A2A",
+          accent: "#F8E9DD",
+          neutral: "#212836",
+          "base-100": "#F8E9DD",
+          info: "#94ddf9",
+          success: "#1d7263",
+          warning: "#bd9614",
+          error: "#e75d50",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
