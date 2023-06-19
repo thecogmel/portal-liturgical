c2a568695bbf9ffff4c7509335bf4468a74681e4
Erick Medeiros <erickmedeiros@b2bit.company>
2023-06-19 19:22:17 -0300

    feat: :chart_with_upwards_trend: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
 import localFont from "next/font/local";
-import Navbar from "@/components/Navbar";
 
 const myFont = localFont({
   src: "../../public/fonts/PoetsenOne-Regular.ttf",
@@ -25,6 +24,7 @@ export default function RootLayout({
       <body className={myFont.className}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
